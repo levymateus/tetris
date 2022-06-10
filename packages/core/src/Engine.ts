8a8c4dd4eaacabf6926d64e7730faba58b452656
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-06-10 11:05:45 -0300

    refactor: make project as monorepo
    split the implementation into packages: core, tetris. The core engine implementation and the tetris
    game implementation.

--- packages/core/src/Engine.ts
+++ packages/core/src/Engine.ts
@@ -4,14 +4,13 @@ import { Viewport } from './Viewport'
 
 export class Engine {
   private instance?: Engine
-  private sceneThree?: SceneTree
 
-  constructor(options?: { viewport?: { width?: number; heigth?: number } }) {
+  constructor(options?: { viewport?: { width?: number; height?: number } }) {
     const viewport = new Viewport(
       options?.viewport?.width || 0,
-      options?.viewport?.heigth || 0
+      options?.viewport?.height || 0
     )
-    this.sceneThree = new SceneTree(viewport)
+    new SceneTree(viewport)
   }
 
   getInstance() {
---
+++ packages/tetris/src/index.ts
@@ -0,0 +1,5 @@
+import Engine from "@tetris/core"
+
+new Engine({ viewport: { width: 300, height: 300 } })
+
+console.log('Hello World!')
